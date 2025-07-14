edbf880dbd0b684f7819ca6437dff96decc86c1f
waqar_haider <waqar.haider@algo.com>
2025-07-15 02:19:14 +0500

    Live Streaming The response for Large Invoice

--- src/main/java/com/example/controller/InvoiceController.java
+++ src/main/java/com/example/controller/InvoiceController.java
@@ -6,6 +6,7 @@ import com.example.service.InvoiceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.List;
 
@@ -52,5 +53,38 @@ public class InvoiceController {
         }
 
         return "Success : Email Sent";
+    }
+
+    @CrossOrigin
+    @GetMapping("/api/v3/invoice")
+    public SseEmitter streamInvoice(
+            @RequestParam String apiKey,
+            @RequestParam String apiSecretKey,
+            @RequestParam String ownerShipId,
+            @RequestParam String subject,
+            @RequestPart MultipartFile file
+    ) {
+        SseEmitter emitter = new SseEmitter();
+        new Thread(() -> {
+            try {
+                List<String> emails = excelReaderService.readEmails(file);
+                int batchSize = 20;
+                for (int i = 0; i < emails.size(); i += batchSize) {
+                    int end = Math.min(i + batchSize, emails.size());
+                    List<String> batchEmail = emails.subList(i, end);
+
+                    int status=invoiceService.sendInvoice(apiKey, apiSecretKey, ownerShipId, subject, batchEmail);
+
+                    if(status!=0)
+                    {
+                        emitter.send("Batch " + (i/batchSize + 1) + " completed.");
+                    }
+                }
+                emitter.complete();
+            } catch (Exception e) {
+                emitter.completeWithError(e);
+            }
+        }).start();
+        return emitter;
     }
 }
